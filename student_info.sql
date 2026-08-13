5b60b0c0452b315f4f6ae40d708dd65ea10af48d
codewithadarsh-hue <adarsh.ksitm@gmail.com>
2026-08-14 00:21:38 +0530

    Add initial student information and queries to student_info.sql

---
+++ student_info.sql
@@ -0,0 +1,5 @@
+INSERT INTO studentinformation VALUES('1','Adarsh T','20','EC','90'), ('2','Saran Kumar','21','BCA','80'),('3','Varshini','20','CS','10'),('4','Prarthana','22','Physi','40');
+SELECT * FROM studentinformation WHERE age>20;
+SELECT * FROM studentinformation WHERE department IN ('CS','Physics');
+SELECT * FROM studentinformation WHERE grade=90;
+SELECT * FROM studentinformation WHERE grade BETWEEN 70 AND 90;
