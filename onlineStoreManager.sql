cc3b5c7f985d082eff4292be6de7e1ed45f0c62e
codewithadarsh-hue <adarsh.ksitm@gmail.com>
2026-08-14 22:37:47 +0530

    Fix formatting of SQL queries in onlineStoreManager.sql

--- onlineStoreManager.sql
+++ onlineStoreManager.sql
@@ -3,5 +3,6 @@ INSERT INTO `storemanager`( `name`, `category`, `price`, `in_stock`) VALUES ('Ap
 SELECT DISTINCT `category`FROM `storemanager`;
 SELECT * FROM `storemanager` WHERE in_stock='Yes' AND price<500;
 SELECT * FROM `storemanager` WHERE in_stock='No' AND price>1000;
-SELECT name,price FROM `storemanager` ORDER BY price DESC;SELECT name,((price*0.18)+price) AS price_with_tax FROM storemanager;
+SELECT name,price FROM `storemanager` ORDER BY price DESC;
+SELECT name,((price*0.18)+price) AS price_with_tax FROM storemanager;
  
